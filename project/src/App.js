9e0d019f28fb8b14695fec9a3dd89c9dddf46a56
dominicho97 <dominic.ho97@gmail.com>
2019-04-23 14:27:25 +0200

    added BrowserRouter, finished setup files

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>hallo</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
